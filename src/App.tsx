ce8c521c4e98756bfb016de41821ba4029397708
KaivnD <KaivnD@hotmail.com>
2020-04-19 00:34:25 +0800

    add:UI framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 import Three from "./scene";
+import { CssBaseline } from "@material-ui/core";
+import { HUD } from "./components/HUD";
 
-function App() {
-  return (
-    <div className="App">
-      <Three />
-    </div>
-  );
+interface AppProp {}
+interface AppState {
+  checkedB: boolean;
+}
+class App extends Component<AppProp, AppState> {
+  constructor(prop: AppProp) {
+    super(prop);
+
+    this.state = {
+      checkedB: false,
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <CssBaseline />
+        <HUD />
+        <Three />
+      </div>
+    );
+  }
 }
 
 export default App;
