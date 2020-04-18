75da8a0c571ef0b8a923843ddb171f340d10122c
KaivnD <KaivnD@hotmail.com>
2020-04-19 00:33:39 +0800

    chore:改写为基类继承

---
+++ src/components/base/BaseScene.tsx
@@ -0,0 +1,259 @@
+import React, { Component } from "react";
+import {
+  WebGLRenderer,
+  PerspectiveCamera,
+  Scene,
+  Mesh,
+  Color,
+  PlaneBufferGeometry,
+  AmbientLight,
+  GridHelper,
+  ShadowMaterial,
+  AxesHelper,
+  DirectionalLight,
+  CameraHelper,
+  Fog,
+  PCFSoftShadowMap,
+  LineBasicMaterial,
+  Raycaster,
+  Vector2,
+  Object3D,
+} from "three";
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
+
+interface BaseSceneState {
+  showHelper: boolean;
+}
+
+export abstract class BaseScene<P, S extends BaseSceneState> extends Component<
+  P,
+  S
+> {
+  el: HTMLDivElement | null = null;
+  scene!: Scene;
+  camera!: PerspectiveCamera;
+  renderer!: WebGLRenderer;
+  directionalLight1!: DirectionalLight;
+  raycaster: Raycaster;
+  mouse: Vector2;
+  outlinePass!: OutlinePass;
+  composer!: EffectComposer;
+  clicked: boolean = false;
+  selectedObject: Object3D | null = null;
+  selectedObjects: Object3D[] = [];
+
+  constructor(prop: P) {
+    super(prop);
+
+    this.raycaster = new Raycaster();
+    this.mouse = new Vector2();
+  }
+
+  componentDidMount() {
+    if (!this.el) return;
+
+    this.scene = new Scene();
+
+    this.godSayNeedLights();
+
+    const retio = window.innerWidth / window.innerHeight;
+
+    this.camera = new PerspectiveCamera(70, retio, 1, 10000);
+    this.camera.position.set(256, 300, 0);
+    this.scene.add(this.camera);
+
+    this.godSayNeedHelp();
+
+    this.renderer = new WebGLRenderer({ antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = PCFSoftShadowMap;
+    this.scene.background = new Color(0x021122);
+    this.scene.fog = new Fog(0x1a2050, 10000, 10000);
+
+    this.godSayNeedControls();
+
+    const planeGeometry = new PlaneBufferGeometry(2000, 2000);
+    planeGeometry.rotateX(-Math.PI / 2);
+    const planeMaterial = new ShadowMaterial({
+      opacity: 0.2,
+    });
+
+    const plane = new Mesh(planeGeometry, planeMaterial);
+    plane.position.y = 0;
+    plane.receiveShadow = true;
+    this.scene.add(plane);
+
+    this.composer = new EffectComposer(this.renderer);
+
+    const renderPass = new RenderPass(this.scene, this.camera);
+    this.composer.addPass(renderPass);
+
+    this.outlinePass = new OutlinePass(
+      new Vector2(window.innerWidth, window.innerHeight),
+      this.scene,
+      this.camera
+    );
+    this.outlinePass.edgeStrength = 2.4;
+    this.outlinePass.edgeGlow = 1;
+    this.outlinePass.edgeThickness = 1;
+    this.outlinePass.visibleEdgeColor = new Color(0x007bd2);
+    this.outlinePass.hiddenEdgeColor = new Color(0x033766);
+    this.composer.addPass(this.outlinePass);
+
+    const bloom = new UnrealBloomPass(new Vector2(1024, 1024), 1, 1, 1);
+    this.composer.addPass(bloom);
+
+    this.el.appendChild(this.renderer.domElement);
+
+    window.addEventListener("resize", () => this.onWindowResize(), false);
+    window.addEventListener("mousemove", (e) => this.onTouchMove(e));
+    window.addEventListener("touchmove", (e) => this.onTouchMove(e));
+    window.addEventListener("mousedown", (e) => this.onTouchStart(e));
+    window.addEventListener("mouseup", (e) => this.onTouchend(e));
+
+    this.createCube();
+
+    this.animate();
+  }
+
+  onTouchend(e: MouseEvent): any {
+    this.clicked = false;
+  }
+
+  onTouchStart(e: MouseEvent): any {
+    if (e.button !== 0) return;
+    this.clicked = true;
+    this.checkIntersection();
+  }
+
+  godSayNeedControls() {
+    // Controls
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.target.set(0, 0, 0);
+    controls.maxDistance = 2000;
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 0.1;
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.4;
+    controls.maxPolarAngle = Math.PI / 2 - 0.15;
+    controls.update();
+  }
+
+  godSayNeedHelp() {
+    const grid = new GridHelper(2000, 100);
+    grid.name = "grid";
+    grid.position.y = 0;
+    grid.visible = true;
+    grid.material = new LineBasicMaterial({
+      opacity: 0.1,
+      transparent: true,
+    });
+    this.scene.add(grid);
+
+    const axes = new AxesHelper(100);
+    axes.name = "axes";
+    axes.visible = this.state.showHelper;
+    axes.position.set(0, 0, 0);
+    this.scene.add(axes);
+  }
+
+  godSayNeedLights() {
+    const ambient = new AmbientLight(0xe8ecff, 1.4);
+    ambient.name = "ambientLight";
+    this.scene.add(ambient);
+
+    this.directionalLight1 = new DirectionalLight(0xfff1f1, 0.7);
+    this.directionalLight1.name = "directionalLight1";
+    this.directionalLight1.position.set(-1000, 600, 1000);
+    this.directionalLight1.castShadow = true;
+    this.scene.add(this.directionalLight1);
+
+    this.directionalLight1.shadow.mapSize.width = 2048;
+    this.directionalLight1.shadow.mapSize.height = 2048;
+
+    const d = 300;
+
+    this.directionalLight1.shadow.camera.right = d;
+    this.directionalLight1.shadow.camera.left = -d;
+    this.directionalLight1.shadow.camera.top = d;
+    this.directionalLight1.shadow.camera.bottom = -d;
+    this.directionalLight1.shadow.camera.near = 0;
+    this.directionalLight1.shadow.camera.far = 5000;
+    // directionalLight1.shadow.camera.fov = 2000;
+
+    const shadowCameraHelper = new CameraHelper(
+      this.directionalLight1.shadow.camera
+    );
+    shadowCameraHelper.visible = false;
+    shadowCameraHelper.name = "directionalLight1Helper";
+    this.scene.add(shadowCameraHelper);
+
+    const directionalLight2 = new DirectionalLight(0x87c0ff, 0.2);
+    directionalLight2.name = "directionalLight2";
+    directionalLight2.position.set(1, 1, -1);
+    this.scene.add(directionalLight2);
+  }
+
+  onTouchMove(e: any) {
+    let x, y;
+
+    if (e.changedTouches) {
+      x = e.changedTouches[0].pageX;
+      y = e.changedTouches[0].pageY;
+    } else {
+      x = e.clientX;
+      y = e.clientY;
+    }
+
+    this.mouse.x = (x / window.innerWidth) * 2 - 1;
+    this.mouse.y = -(y / window.innerHeight) * 2 + 1;
+
+    this.checkIntersection();
+  }
+
+  lastSelectedObj?: Object3D;
+
+  checkIntersection() {
+    if (!this.scene) return;
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+
+    const intersects = this.raycaster.intersectObjects([this.scene], true);
+
+    if (intersects.length > 0) {
+      const obj = intersects[0].object;
+
+      if (this.clicked) this.selectedObject = obj;
+      this.outlinePass.selectedObjects = [obj];
+    }
+  }
+
+  onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  abstract createCube(): void;
+
+  animate = () => {
+    requestAnimationFrame(this.animate);
+    this.composer.render();
+  };
+
+  componentWillUnmount() {
+    if (!this.el) return;
+    this.el.removeChild(this.renderer.domElement);
+  }
+
+  render() {
+    return <div ref={(el) => (this.el = el)} />;
+  }
+}
--- src/scene.tsx
+++ src/scene.tsx
@@ -1,151 +1,38 @@
-import React, { Component } from "react";
 import {
-  WebGLRenderer,
-  PerspectiveCamera,
-  Scene,
   Mesh,
   Color,
-  PlaneBufferGeometry,
-  AmbientLight,
-  GridHelper,
-  ShadowMaterial,
-  AxesHelper,
   Shape,
   Vector2,
-  DirectionalLight,
   ExtrudeBufferGeometry,
-  CameraHelper,
-  Fog,
-  PCFSoftShadowMap,
   MeshStandardMaterial,
-  LineBasicMaterial,
   Vector3,
   SphereGeometry,
 } from "three";
 
+import { BaseScene } from "./components/base/BaseScene";
+
 import model from "./abc.json";
+import eve from "./libs/eve";
 
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+interface ThreeState {
+  showHelper: boolean;
+  clicked: boolean;
+}
 
-interface ThreeProp {}
+class Three extends BaseScene<{}, ThreeState> {
+  constructor(prop: {}) {
+    super(prop);
 
-class Three extends Component<ThreeProp> {
-  el: HTMLDivElement | null = null;
-  scene!: Scene;
-  camera!: PerspectiveCamera;
-  renderer!: WebGLRenderer;
+    this.state = {
+      showHelper: true,
+      clicked: false,
+    };
 
-  componentDidMount() {
-    if (!this.el) return;
-
-    this.scene = new Scene();
-
-    const ambient = new AmbientLight(0xe8ecff, 1.4);
-    ambient.name = "ambientLight";
-    this.scene.add(ambient);
-
-    const directionalLight1 = new DirectionalLight(0xfff1f1, 0.7);
-    directionalLight1.name = "directionalLight1";
-    directionalLight1.position.set(-1000, 600, 1000);
-    directionalLight1.castShadow = true;
-    this.scene.add(directionalLight1);
-
-    directionalLight1.shadow.mapSize.width = 2048;
-    directionalLight1.shadow.mapSize.height = 2048;
-
-    const d = 300;
-
-    directionalLight1.shadow.camera.right = d;
-    directionalLight1.shadow.camera.left = -d;
-    directionalLight1.shadow.camera.top = d;
-    directionalLight1.shadow.camera.bottom = -d;
-    directionalLight1.shadow.camera.near = 0;
-    directionalLight1.shadow.camera.far = 5000;
-    // directionalLight1.shadow.camera.fov = 2000;
-
-    const shadowCameraHelper = new CameraHelper(
-      directionalLight1.shadow.camera
-    );
-    shadowCameraHelper.visible = false;
-    shadowCameraHelper.name = "directionalLight1Helper";
-    this.scene.add(shadowCameraHelper);
-
-    const directionalLight2 = new DirectionalLight(0x87c0ff, 0.2);
-    directionalLight2.name = "directionalLight2";
-    directionalLight2.position.set(1, 1, -1);
-    this.scene.add(directionalLight2);
-
-    this.camera = new PerspectiveCamera(
-      70,
-      window.innerWidth / window.innerHeight,
-      1,
-      10000
-    );
-    this.camera.position.set(256, 300, 0);
-    this.scene.add(this.camera);
-
-    const grid = new GridHelper(2000, 100);
-    grid.name = "grid";
-    grid.position.y = 0;
-    grid.visible = true;
-    grid.material = new LineBasicMaterial({
-      opacity: 0.1,
-      transparent: true,
+    eve.on("changeLightPos", (v) => {
+      if (!this.directionalLight1) return;
+      this.directionalLight1.position.setY(v);
     });
-    this.scene.add(grid);
-
-    const axes = new AxesHelper(100);
-    axes.name = "axes";
-    axes.visible = true;
-    axes.position.set(0, 0, 0);
-    this.scene.add(axes);
-
-    this.renderer = new WebGLRenderer({ antialias: true });
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.shadowMap.enabled = true;
-    this.renderer.shadowMap.type = PCFSoftShadowMap;
-    this.scene.background = new Color(0x0f131a);
-    this.scene.fog = new Fog(0x1a2050, 10000, 10000);
-
-    // Controls
-    const controls = new OrbitControls(this.camera, this.renderer.domElement);
-    controls.target.set(0, 0, 0);
-    controls.maxDistance = 2000;
-    controls.autoRotate = true;
-    controls.autoRotateSpeed = 0.1;
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.4;
-    controls.maxPolarAngle = Math.PI / 2 - 0.15;
-    controls.update();
-
-    const planeGeometry = new PlaneBufferGeometry(2000, 2000);
-    planeGeometry.rotateX(-Math.PI / 2);
-    const planeMaterial = new ShadowMaterial({
-      opacity: 0.2,
-    });
-
-    const plane = new Mesh(planeGeometry, planeMaterial);
-    plane.position.y = 0;
-    plane.receiveShadow = true;
-    this.scene.add(plane);
-
-    this.el.appendChild(this.renderer.domElement);
-
-    window.addEventListener("resize", () => this.onWindowResize(), false);
-
-    this.createCube();
-
-    this.animate();
   }
-
-  onWindowResize() {
-    this.camera.aspect = window.innerWidth / window.innerHeight;
-    this.camera.updateProjectionMatrix();
-
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-  }
-
   createCube() {
     const material = new MeshStandardMaterial({
       color: new Color(0xffffff),
@@ -156,6 +43,7 @@ class Three extends Component<ThreeProp> {
       // opacity: 0.7,
     });
     // const lineMat = new LineBasicMaterial({ color: new Color(0x000000) });
+
     model.a.forEach((a) => {
       const pts: Vector2[] = [];
 
@@ -170,7 +58,7 @@ class Three extends Component<ThreeProp> {
         depth: Number(a.h) * a.c,
       });
       building.rotateX(-Math.PI / 2);
-      building.translate(-700, 0, 150);
+      building.translate(-800, 0, 150);
 
       const buildingMesh = new Mesh(building, material);
       buildingMesh.name = "buildings";
@@ -200,7 +88,7 @@ class Three extends Component<ThreeProp> {
         depth: 0.45,
       });
       building.rotateX(-Math.PI / 2);
-      building.translate(-700, 0, 150);
+      building.translate(-800, 0, 150);
 
       const buildingMesh = new Mesh(building, material);
       buildingMesh.name = "block";
@@ -216,26 +104,13 @@ class Three extends Component<ThreeProp> {
 
       tree.translate(loc.x, loc.y, loc.z);
       tree.rotateX(-Math.PI / 2);
-      tree.translate(-700, 0, 150);
+      tree.translate(-800, 0, 150);
 
       const treeMesh = new Mesh(tree, material);
       treeMesh.castShadow = true;
       treeMesh.receiveShadow = true;
       this.scene.add(treeMesh);
     });
-  }
-
-  animate = () => {
-    requestAnimationFrame(this.animate);
-    this.renderer.render(this.scene, this.camera);
-  };
-
-  componentWillUnmount() {
-    if (!this.el) return;
-    this.el.removeChild(this.renderer.domElement);
-  }
-  render() {
-    return <div ref={(el) => (this.el = el)} />;
   }
 }
 export default Three;
