dfe14e54485e51dfdf05ab5e7198c88835d4c1ee
KaivnD <KaivnD@hotmail.com>
2020-04-19 02:51:05 +0800

    add:section card

--- src/components/HUD.tsx
+++ src/components/HUD.tsx
@@ -1,10 +1,55 @@
 import React, { Component } from "react";
-import { Slider, SvgIcon } from "@material-ui/core";
+import {
+  Slider,
+  SvgIcon,
+  AppBar,
+  Tabs,
+  Tab,
+  Typography,
+  Box,
+} from "@material-ui/core";
 import { StyleSheet, css } from "aphrodite";
+import { Phone, Favorite, PersonPin } from "@material-ui/icons";
 
 import eve from "../libs/eve";
+import theme from "../themes/default";
 
-export class HUD extends Component {
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Box
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      123
+    </Box>
+  );
+}
+
+interface HUDState {
+  page: number;
+}
+
+export class HUD extends Component<{}, HUDState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      page: 0,
+    };
+  }
   render() {
     const styles = StyleSheet.create({
       root: {
@@ -62,9 +107,12 @@ export class HUD extends Component {
     function valuetext(value: number) {
       return `${value}°C`;
     }
+
     return (
-      <div className={css(styles.root)}>
-        {/* <Switch
+      <>
+        {this.props.children}
+        <div className={css(styles.root)}>
+          {/* <Switch
             checked={this.state.checkedB}
             onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
               this.setState({ checkedB: event.target.checked });
@@ -74,37 +122,62 @@ export class HUD extends Component {
             name="checkedB"
             inputProps={{ "aria-label": "primary checkbox" }}
           /> */}
-        <div className={css(styles.wrap)}>
-          <div>
-            <SvgIcon style={{ fontSize: "128px" }} viewBox="0 0 1024 1024">
-              <g
-                style={{
-                  fill: "rgba(255,255,255,0.1)",
-                  stroke: "#fff",
-                  strokeWidth: 3,
-                }}
+          <div className={css(styles.wrap)}>
+            <div>
+              <SvgIcon
+                style={{ fontSize: "128px", position: "absolute" }}
+                viewBox="0 0 1024 1024"
               >
-                <path d="M371.5,612.5v-200a40,40,0,0,1,80,0v200a160,160,0,0,0,320,0v-400a40,40,0,0,1,80,0v400c0,132.55-107.45,240-240,240S371.5,745.05,371.5,612.5Z" />
-                <path d="M651.5,412.5v40a40,40,0,0,1-80,0v-40a160,160,0,0,0-320,0v400a40,40,0,0,1-80,0v-400c0-132.55,107.45-240,240-240S651.5,280,651.5,412.5Z" />
-                <path d="M571.5,612.3a40,40,0,1,1,39.8,40.2,40,40,0,0,1-39.8-40.2Z" />
-              </g>
-            </SvgIcon>
-          </div>
-          <div className={css(styles.dataPanel)}>
-            <Slider
-              defaultValue={20}
-              getAriaValueText={valuetext}
-              aria-labelledby="discrete-slider-custom"
-              step={10}
-              valueLabelDisplay="auto"
-              marks={marks}
-              onChangeCommitted={(e, v) => {
-                eve.emit("changeLightPos", v);
-              }}
-            />
+                <g
+                  style={{
+                    fill: "rgba(255,255,255,0.1)",
+                    stroke: "#fff",
+                    strokeWidth: 3,
+                  }}
+                >
+                  <path d="M371.5,612.5v-200a40,40,0,0,1,80,0v200a160,160,0,0,0,320,0v-400a40,40,0,0,1,80,0v400c0,132.55-107.45,240-240,240S371.5,745.05,371.5,612.5Z" />
+                  <path d="M651.5,412.5v40a40,40,0,0,1-80,0v-40a160,160,0,0,0-320,0v400a40,40,0,0,1-80,0v-400c0-132.55,107.45-240,240-240S651.5,280,651.5,412.5Z" />
+                  <path d="M571.5,612.3a40,40,0,1,1,39.8,40.2,40,40,0,0,1-39.8-40.2Z" />
+                </g>
+              </SvgIcon>
+            </div>
+            <div className={css(styles.dataPanel)}>
+              <Tabs
+                value={this.state.page}
+                onChange={(e, v) => this.setState({ page: v })}
+                variant="fullWidth"
+                indicatorColor="secondary"
+                textColor="secondary"
+                aria-label="icon label tabs example"
+              >
+                <Tab icon={<Phone />} label="RECENTS" />
+                <Tab icon={<Favorite />} label="FAVORITES" />
+                <Tab icon={<PersonPin />} label="NEARBY" />
+              </Tabs>
+              <TabPanel value={this.state.page} index={0} dir={theme.direction}>
+                Item One
+              </TabPanel>
+              <TabPanel value={this.state.page} index={1} dir={theme.direction}>
+                Item Two
+              </TabPanel>
+              <TabPanel value={this.state.page} index={2} dir={theme.direction}>
+                Item Three
+              </TabPanel>
+              {/* <Slider
+                defaultValue={20}
+                getAriaValueText={valuetext}
+                aria-labelledby="discrete-slider-custom"
+                step={10}
+                valueLabelDisplay="auto"
+                marks={marks}
+                onChangeCommitted={(e, v) => {
+                  eve.emit("changeLightPos", v);
+                }}
+              /> */}
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
